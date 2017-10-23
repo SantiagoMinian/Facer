3e9b19cad90d2c89768cd7c3ab8716ff8489b565
SantiagoMinian <minian.santiago@gmail.com>
2017-10-23 10:43:57 -0300

    Create new processor that takes as much photos as possible

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-import time
 import socket
+import time
 
 import cv2
-import datetime
 import numpy
 from pymongo import MongoClient
 
@@ -14,14 +13,13 @@ MIN_HEIGHT = 180
 HOST = "127.0.0.1"
 PORT = 5000
 
-# TODO chequear TODOS
+
 # TODO hacer flexible cantidad de fotos que se van a necesitar para ver bien a una persona
 def main():
-
     # name_encodings is a dictionary with name as key and amount of encodings as value
     (known_encodings, names, name_encodings, people_collection) = init()
     server_socket = init_server()
-    cap = init_camera()
+    video_capture = init_camera()
 
     while True:
         connection, address = server_socket.accept()
@@ -42,31 +40,24 @@ def main():
             while time.time() < t_end:
 
                 # Saco foto
-                ret, frame = cap.read()
-                print("Took photo")
+                frame = take_photo(video_capture)
+                if frame is None:
+                    continue
 
                 # Recorto frame para evitar agarrar otras personas
-                height, width = frame.shape[:2]
-
-                start_x = int(width/4)
-                end_x = start_x + int(width/2)
-
-                frame = frame[0:height, start_x:end_x]
+                frame = resize_frame(frame)
 
                 # Locateo
                 face_locations = locate_faces(frame)
 
                 # Encodeo cara
-                faces = len(face_locations)
-                if faces == 1:
+                check = check_locations(face_locations, connection)
+
+                if check:
                     face_encoding = face_recognition.face_encodings(frame, face_locations)[0]
-                    print("Encoded face")
-
-                elif faces == 0:
-                    print("No faces detected")
-
                 else:
-                    print("Too many faces detected")
+                    # TODO: sumar uno a contador de fotos que no sirvieron
+                    continue
 
                 # Guardo encoding
                 person_encodings.append(face_encoding)
@@ -88,22 +79,63 @@ def main():
                     else:
                         percentage_average[key] = value
 
-            max_percentage = max(percentage_average, key=percentage_average.get)
+            for key, value in percentage_average.items():
+                percentage_average[key] /= len(person_percentages)
+
+            max_percentage_name = max(percentage_average, key=percentage_average.get)
+            max_percentage = percentage_average[max_percentage_name]
 
             # Defino tolerancia
             # Segun tolerancia defino si conozco o no a esa persona
             # TODO: Adjust tolerance
             tolerance = 0.7
 
-            # Si no conozco: recorro person_encodings y apendeo todos a known encodings, apendeando tambien el nombre a
-            # names y agrego una entrada a name_encodings con el nombre y la cantidad de encodings que apendee
-            # tambien guardo en la base. ver to do de init()
-            if max_percentage >= tolerance:
-                None
+            # Chequeo si conozco
+            if max_percentage <= tolerance:
                 # Persona nueva
+                for encoding in person_encodings:
+                    # TODO: poner bien el nombre
+                    # image_name = "face{}{:%Y-%m-%d%H:%M:%S}.jpg".format(name, datetime.datetime.now())
+                    # TODO: conseguir foto de la cara(una aleatoria??? o se pueden guardar todas y despues ver a mano
+                    # TODO que onda, total el ganador va a ser a mano) y guardarla
+                    # (top, right, bottom, left) = face_location
+                    # x = left
+                    # y = top
+                    # w = right - left
+                    # h = bottom - top
+                    #
+                    # face_image = frame[y:y + h, x:x + w]
+                    # cv2.imwrite(image_name, face_image)
+
+                    known_encodings.append(encoding)
+                    names.append(name)
+
+                    print("Unknown face, added {}".format(name))
+                    connection.send(("Unknown face, added {}".format(name)).encode())
+
+                name = "V#{}".format(len(name_encodings) + 1)
+                person_encodings = [person_encoding.toList() for person_encoding in person_encodings]
+
+                people_collection.insert_one(
+                    {
+                        "name": name,
+                        "encodings": person_encodings
+                        # TODO: save image path
+                    }
+                )
             else:
-                None
+                # TODO: hablar con nico a ver si nos conviene agregar nuevs encodings si viene una persona conocida
                 # Conocida
+                print("Known face of {}".format(max_percentage_name))
+                connection.send(("Known face of {}".format(max_percentage_name)).encode())
+
+
+def resize_frame(frame):
+    height, width = frame.shape[:2]
+    start_x = int(width / 4)
+    end_x = start_x + int(width / 2)
+    frame = frame[0:height, start_x:end_x]
+    return frame
 
 
 def init():
@@ -111,16 +143,22 @@ def init():
     names = []
     name_encodings = {}
 
-    client = MongoClient()
+    uri = "mongodb://superadmin:12345678@localhost:27017/admin?authMechanism=SCRAM-SHA-1"
+    client = MongoClient(uri)
     db = client.facer
 
     people_collection = db.people
 
     cursor = people_collection.find()
-    # TODO: populate name encodings and fix the way we save people in the database
     for document in cursor:
-        known_encodings.append(numpy.array(document["encoding"]))
-        names.append(document["name"])
+        encoding_array = numpy.array(document["encodings"])
+        person_name = document["name"]
+
+        name_encodings[person_name] = len(encoding_array)
+
+        for face_encoding in encoding_array:
+            known_encodings.append(face_encoding)
+            names.append(person_name)
 
     return known_encodings, names, name_encodings, people_collection
 
@@ -136,10 +174,20 @@ def init_server():
 
 
 def init_camera():
-    cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1440)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 810)
+    video_capture = cv2.VideoCapture(0)
+    video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1440)
+    video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 810)
     print("Opened video capture")
+    return video_capture
+
+
+def take_photo(video_capture):
+    ret, frame = video_capture.read()
+    if not ret:
+        print("Couldn't take photo")
+        return None
+    print("Took photo")
+    return frame
 
 
 def locate_faces(frame):
@@ -161,16 +209,20 @@ def locate_faces(frame):
 
     return face_locations
 
-    # TODO: chequear y reubicar
-    # if len(face_locations) == 0:
-    #     print("No faces detected")
-    #     connection.send("No faces detected".encode())
-    #     return
-    #
-    # if len(face_locations) > 1:
-    #     print("Too many faces detected")
-    #     connection.send("Too many faces detected".encode())
-    #     return
+
+def check_locations(face_locations, connection):
+    faces = len(face_locations)
+    if faces == 1:
+        print("Encoded face")
+        return True
+    elif faces == 0:
+        print("No faces detected")
+        connection.send("No faces detected".encode())
+        return False
+    else:
+        print("Too many faces detected")
+        connection.send("Too many faces detected".encode())
+        return False
 
 
 def compare_encoding(known_encodings, face_encoding, name_encodings, names):
@@ -188,54 +240,9 @@ def compare_encoding(known_encodings, face_encoding, name_encodings, names):
     for key, value in percentages.items():
         percentages[key] /= name_encodings[key]
 
-    percentages = sorted(percentages, key=percentages.get, reverse=True)[:5]
+    percentages = {key: percentages[key] for key in (sorted(percentages, key=percentages.get, reverse=True)[:5])}
 
     return percentages
-
-
-def process_face_encoding(face_encoding, frame, face_location, known_encodings, names, people_collection, connection):
-    matches = face_recognition.compare_faces(known_encodings, face_encoding, 0.6)
-    known = False
-    print("Compared faces")
-    for i, match in enumerate(matches):
-        if match:
-            known = True
-
-            print("Known face of {}".format(names[i]))
-            connection.send(("Known face of {}".format(names[i])).encode())
-
-            break
-
-    if not known:
-        print("Unknown face")
-
-        (top, right, bottom, left) = face_location
-        x = left
-        y = top
-        w = right - left
-        h = bottom - top
-
-        face_image = frame[y:y + h, x:x + w]
-
-        add_visitor(face_image, names, known_encodings, face_encoding, people_collection, connection)
-
-
-def add_visitor(face_image, names, known_encodings, face_encoding, people_collection, connection):
-    name = "V#{}".format(len(names) + 1)
-    known_encodings.append(face_encoding)
-    names.append(name)
-    n = "face{:%Y-%m-%d%H:%M:%S}.jpg".format(datetime.datetime.now())
-    cv2.imwrite(n, face_image)
-
-    people_collection.insert_one(
-        {
-            "name": name,
-            "encoding": face_encoding.tolist()
-        }
-    )
-
-    print("Unknown face, added {}".format(name))
-    connection.send(("Unknown face, added {}".format(name)).encode())
 
 
 main()
